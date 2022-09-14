e9cecbfdda6f067bf46244ef98cf251d79e8322d
Ilkhom Khafizov <ilhomhafizov@MacBook-Pro-Ilhom.local>
2022-09-14 09:57:08 +0500

    add delete question func

--- handlers/admin/delete.py
+++ handlers/admin/delete.py
@@ -3,14 +3,14 @@ from aiogram.dispatcher.filters import Command
 
 from filters import IsPrivate
 from loader import dp
-from utils.check_admin import check_auth
+from utils.check_admin import check_admin
 from utils.db_api import question_commands
 from utils.misc import rate_limit
 
 
-@rate_limit(limit=120, key='/delete')
+@rate_limit(limit=12, key='/delete')
 @dp.message_handler(IsPrivate(), Command('delete'))
-@check_auth
+@check_admin
 async def delete_command(message: types.Message):
     questions = await question_commands.select_all_questions()
     if len(questions) > 0:
